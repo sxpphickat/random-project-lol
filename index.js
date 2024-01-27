3e4f323226d4748e46b4269ca87e9896e873479f
sxpphickat <sphhhsh@gmail.com>
2024-01-27 18:44:13 -0300

    feat: checking player elo

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import routes from './riot_endpoints.json' assert { type: 'json' }
 
+const header = {
+    headers: {
+      "X-Riot-Token": process.env.API_TOKEN
+    }
+};
+
 async function getPuuidByRiotId(gameName, tagLine) {
   const accountEndPoint = `${routes['endpoint']['puuid']}${gameName}/${tagLine}`;
   const request = new URL(`https://${routes['region']['AMERICAS']}${accountEndPoint}?api_key=${process.env.API_TOKEN}`);
@@ -22,15 +28,11 @@ async function getPuuidByRiotId(gameName, tagLine) {
 
 const puuid = await getPuuidByRiotId('Sapphic Kat', 'BR1');
 
-console.log(puuid);
+//console.log(puuid);
 
 async function getSummonerByPuuid() {
   const request = new URL(`https://${routes['server']['BR1']}${routes['endpoint']['summoner']}${puuid.puuid}`); 
-  const header = {
-    headers: {
-      "X-Riot-Token": process.env.API_TOKEN
-    }
-  };
+
 
   const res = await fetch(request, header)
     .then(res => {
@@ -38,7 +40,7 @@ async function getSummonerByPuuid() {
         console.log(res);
         throw new Error('summoner');
       }
-      return res;
+      return res.json();
     })
     .then(data => {
       return data;
@@ -51,5 +53,27 @@ async function getSummonerByPuuid() {
 
 const summoner = await getSummonerByPuuid();
 
-console.log(summoner);
+//console.log(summoner);
 
+async function getEntriesBySummoner() {
+  const request = new URL(`https://${routes['server']['BR1']}${routes['endpoint']['entryes']}${summoner.id}`);
+  
+  const res = await fetch(request, header)
+  .then(res => {
+      if (!res.ok) {
+        throw new Error('entryes');
+      }
+      return res.json();
+    })
+  .then(data => data)
+  .catch(error => {
+      console.error('Error: ', error);
+    });
+  return res;
+}
+
+const entryes = await getEntriesBySummoner();
+
+
+//console.log(entryes);
+console.log(`${entryes[0].summonerName}, your elo is: ${entryes[0].tier} - ${entryes[0].rank}`);
