727735c64927eef77b115fee0b7ad86c7b454d7e
sxpphickat <sphhhsh@gmail.com>
2024-01-27 16:44:18 -0300

    feat: get summuner funciton

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 import routes from './riot_endpoints.json' assert { type: 'json' }
 
-
-async function getPuuid(gameName, tagLine) {
-  const accountEndPoint = `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
+async function getPuuidByRiotId(gameName, tagLine) {
+  const accountEndPoint = `${routes['endpoint']['puuid']}${gameName}/${tagLine}`;
   const request = new URL(`https://${routes['region']['AMERICAS']}${accountEndPoint}?api_key=${process.env.API_TOKEN}`);
-  console.log(request);
-  
-  await fetch(request)
+
+  const res = await fetch(request)
     .then((res) => {
     if(!res.ok) {
       throw new Error('error no 666');
-    }
+      }
       return res.json();
     })
     .then(data => {
-      console.log(data);
+      return data;
     })
     .catch(error => {
       console.error('Error: ', error);
-    })
+    });
+  return res;
 };
 
-const puuid = await getPuuid('Sapphic Kat', 'BR1');
+const puuid = await getPuuidByRiotId('Sapphic Kat', 'BR1');
+
+console.log(puuid);
+
+async function getSummonerByPuuid() {
+  const request = new URL(`https://${routes['server']['BR1']}${routes['endpoint']['summoner']}${puuid.puuid}`); 
+  const header = {
+    headers: {
+      "X-Riot-Token": process.env.API_TOKEN
+    }
+  };
+
+  const res = await fetch(request, header)
+    .then(res => {
+      if (!res.ok) {
+        console.log(res);
+        throw new Error('summoner');
+      }
+      return res;
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.error('Error: ', error);
+    });
+  return res;
+}
+
+const summoner = await getSummonerByPuuid();
+
+console.log(summoner);
+
